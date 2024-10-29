28c337107566356795f7d7164b7593a45a70d926
ThatGhost <stargamer.me@gmail.com>
2024-10-29 12:31:00 +0100

    added timer page and fonts

---
+++ Jenny-V2/Pages/Timers.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jenny_V2.Pages
+{
+    /// <summary>
+    /// Interaction logic for Timers.xaml
+    /// </summary>
+    public partial class Timers : Page
+    {
+        public Timers()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnModeSwitch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
